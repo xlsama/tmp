3edb778f367e950a7c60116d4a36673d82bad6d3
xlsama <im.xlsama@gmail.com>
2025-08-07 17:54:40 +0800

    chore: update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,34 @@
-import tailwindcss from "@tailwindcss/vite";
-import { definePreset } from "@primeuix/themes";
-import Aura from "@primeuix/themes/aura";
+import tailwindcss from '@tailwindcss/vite'
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@primevue/nuxt-module',
+  ],
+  imports: {
+    presets: [{ from: 'dayjs', imports: [{ name: 'default', as: 'dayjs' }] }],
+  },
+  devtools: { enabled: true },
   app: {
     head: {
-      title: "Nuxt App",
+      title: 'Nuxt App',
     },
   },
-  compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
-  css: ["~/assets/css/main.css"],
-  modules: [
-    "@nuxt/eslint",
-    "@nuxt/fonts",
-    "@nuxt/image",
-    "@primevue/nuxt-module",
-  ],
+  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
   primevue: {
     options: {
       theme: {
@@ -26,27 +37,21 @@ export default defineNuxtConfig({
         preset: definePreset(Aura, {
           semantic: {
             primary: {
-              50: "#f2f7f5",
-              100: "#c2dad1",
-              200: "#91bcad",
-              300: "#619f89",
-              400: "#308164",
-              500: "#006440",
-              600: "#005536",
-              700: "#00462d",
-              800: "#003723",
-              900: "#00281a",
-              950: "#001910",
+              50: '#f2f7f5',
+              100: '#c2dad1',
+              200: '#91bcad',
+              300: '#619f89',
+              400: '#308164',
+              500: '#006440',
+              600: '#005536',
+              700: '#00462d',
+              800: '#003723',
+              900: '#00281a',
+              950: '#001910',
             },
           },
         }),
       },
     },
   },
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  imports: {
-    presets: [{ from: "dayjs", imports: [{ name: "default", as: "dayjs" }] }],
-  },
-});
+})
