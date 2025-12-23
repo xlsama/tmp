ec93a50a276000973093c46a4c0925cd688e4db0
xlsama <im.xlsama@gmail.com>
2025-12-23 18:04:56 +0800

    u

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,13 @@
 <template>
   <div class="space-y-5">
     <h2 class="text-2xl font-bold">Dashboard</h2>
-    <section class="grid gap-4">
-      <UCard v-for="item in 50" :key="item"> {{ item }} </UCard>
+    <section class="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
+      <UCard v-for="item in 50" :key="item">
+        <template #header>
+          <h3 class="">Card {{ item }}</h3>
+        </template>
+        <p class="py-10 text-sm text-gray-500">This is a card body {{ item }}</p>
+      </UCard>
     </section>
   </div>
 </template>
