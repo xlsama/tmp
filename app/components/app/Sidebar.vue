a2d181e8e8975da36a81a64c518ac600875b5a37
xlsama <im.xlsama@gmail.com>
2025-12-23 18:01:32 +0800

    u

--- app/components/app/Sidebar.vue
+++ app/components/app/Sidebar.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <aside class="border-r border-default p-4">
+  <aside class="overflow-y-auto border-r border-default p-4">
     <h2 class="text-xl font-bold">Sidebar</h2>
   </aside>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="space-y-5">
     <h2 class="text-2xl font-bold">Dashboard</h2>
+    <section class="grid gap-4">
+      <UCard v-for="item in 50" :key="item"> {{ item }} </UCard>
+    </section>
   </div>
 </template>
